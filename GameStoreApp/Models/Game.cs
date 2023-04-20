5fb1748954ebabb51da20195885ee55bf8bea966
Owen2533667 <owenhughes67@gmail.com>
2023-04-21 00:40:32 +0100

    Minor changes to game model
    Minor changes to game model

--- GameStoreApp/Models/Game.cs
+++ GameStoreApp/Models/Game.cs
@@ -8,25 +8,25 @@ namespace GameStoreApp.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
         public DateTime ReleaseDate { get; set; }
         public double Price { get; set; }
-        public string imageURL { get; set; }
+        public string? ImageURL { get; set; }
         public GameGenre GameGenre { get; set; }
 
         //Relationships
-        public List<VoiceActor_Game> VoiceActors_Games { get; set; }
+        public List<VoiceActor_Game>? VoiceActors_Games { get; set; }
 
         //Developer
         public int GameDeveloperId { get; set; }
         [ForeignKey("GameDeveloperId")]
-        public GameDeveloper GameDeveloper { get; set; }
+        public GameDeveloper? GameDeveloper { get; set; }
 
         //Publisher
         public int GamePublisherId { get; set; }
         [ForeignKey("GamePublisherId")]
-        public GamePublisher GamePublisher { get; set; }
+        public GamePublisher? GamePublisher { get; set; }
 
 
     }
