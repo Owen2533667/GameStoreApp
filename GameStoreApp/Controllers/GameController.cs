edc8bc080eaaccfacabfa156a70cdaa2fc61a1cf
Owen2533667 <owenhughes67@gmail.com>
2023-04-20 20:25:49 +0100

    Controllers created
    The creation of controllers

---
+++ GameStoreApp/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using GameStoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStoreApp.Controllers
+{
+    public class GameController : Controller
+    {
+        //private readonly attribute to get db context
+        private readonly GameStoreAppDbContext _context;
+
+        //constructor
+        public GameController(GameStoreAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Games.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ GameStoreApp/Controllers/GameDeveloperController.cs
@@ -0,0 +1,24 @@
+﻿using GameStoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStoreApp.Controllers
+{
+    public class GameDeveloperController : Controller
+    {
+        //private readonly attribute to get db context
+        private readonly GameStoreAppDbContext _context;
+
+        //constructor
+        public GameDeveloperController(GameStoreAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.GameDevelopers.ToListAsync();
+            return View(data);
+        }
+    }
+}
