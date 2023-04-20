eefa40242c3019081eb5ddd12e3519bfca887633
Owen2533667 <owenhughes67@gmail.com>
2023-04-20 20:26:32 +0100

    Display annotation tag to attributes
    Display annotation tag to attributes

--- GameStoreApp/Models/GamePublisher.cs
+++ GameStoreApp/Models/GamePublisher.cs
@@ -6,8 +6,11 @@ namespace GameStoreApp.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Publisher Logo")]
         public string Logo { get; set; }
+        [Display(Name = "Publisher Name")]
         public string Name { get; set; }
+        [Display(Name = "Publisher Description")]
         public string Description { get; set; }
 
         //Relationship
