7c06d201892e7b420e933631af6881977c0f2692
Owen2533667 <owenhughes67@gmail.com>
2023-04-23 22:11:58 +0100

    GetAllAsync signature and implementation
    Added a getAllAsync that accepts parameters that will be included properties.

--- GameStoreApp/Data/Base/EntityBaseRepository.cs
+++ GameStoreApp/Data/Base/EntityBaseRepository.cs
@@ -1,6 +1,7 @@
 ﻿using GameStoreApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq.Expressions;
 
 namespace GameStoreApp.Data.Base
 {
@@ -28,8 +29,15 @@ namespace GameStoreApp.Data.Base
             await _context.SaveChangesAsync();
         }
 
+        //Asynchronous method to get all from the database table <T>
         public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
 
+        public async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            query = includeProperties.Aggregate(query, (current, includeProperties) => current.Include(includeProperties));
+            return await query.ToListAsync();
+        }
 
         public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
 
