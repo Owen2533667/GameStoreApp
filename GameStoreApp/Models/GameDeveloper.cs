782debff3b2cce5f4903c89a9f555dd223eae794
Owen2533667 <owenhughes67@gmail.com>
2023-04-23 22:19:26 +0100

    Update GameDeveloper.cs

--- GameStoreApp/Models/GameDeveloper.cs
+++ GameStoreApp/Models/GameDeveloper.cs
@@ -7,15 +7,19 @@ namespace GameStoreApp.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Display(Name = "Developer Logo")]
         [Required(ErrorMessage = "The Logo is a required field")]
         public string? Logo { get; set; }
+
         [Display(Name = "Developer Name")]
         [Required(ErrorMessage = "The Name is a required field")]
         public string? Name { get; set; }
+
         [Display(Name = "Developer Description")]
         [Required(ErrorMessage = "The Description is a required field")]
         public string? Description { get; set; }
+
 
         //Relationships
         public List<Game>? Games { get; set; }
