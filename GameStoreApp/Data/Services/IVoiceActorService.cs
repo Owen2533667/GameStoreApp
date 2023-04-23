c15e8dacd346aee9cec82ee5fe7852428a001ffa
Owen2533667 <owenhughes67@gmail.com>
2023-04-23 01:34:17 +0100

    VoiceActorService
    VoiceActor service interface that uses the entity base repository.

---
+++ GameStoreApp/Data/Services/IVoiceActorService.cs
@@ -0,0 +1,10 @@
+﻿using GameStoreApp.Data.Base;
+using GameStoreApp.Models;
+
+namespace GameStoreApp.Data.Services
+{
+    public interface IVoiceActorService:IEntityBaseRepository<VoiceActor>
+    {
+
+    }
+}
---
+++ GameStoreApp/Data/Services/VoiceActorService.cs
@@ -0,0 +1,13 @@
+﻿using GameStoreApp.Data.Base;
+using GameStoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStoreApp.Data.Services
+{
+    public class VoiceActorService : EntityBaseRepository<VoiceActor>, IVoiceActorService
+    {
+
+
+        public VoiceActorService(GameStoreAppDbContext context) : base(context) { }
+    }
+}
