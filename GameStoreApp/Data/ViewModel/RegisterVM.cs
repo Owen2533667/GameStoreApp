9ae713c11408c18a2d4ec9a147ed58fbb5b3b081
Owen2533667 <owenhughes67@gmail.com>
2023-04-24 21:37:11 +0100

    Create RegisterVM.cs

---
+++ GameStoreApp/Data/ViewModel/RegisterVM.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace GameStoreApp.Data.ViewModel
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        [Display(Name = "First Name")]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Family Name is required")]
+        [Display(Name = "Family Name")]
+        [DataType(DataType.Text)]
+        public string FamilyName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Comfirm Password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Comfirm Password")]
+        [Compare("Password", ErrorMessage = "Password do not match!")]
+        public string ComfirmPassword { get; set; }
+    }
+}
