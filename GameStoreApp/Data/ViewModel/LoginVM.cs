100d812b280f47b69fc86aa2fb621b98e234ccb6
Owen2533667 <owenhughes67@gmail.com>
2023-04-24 21:37:08 +0100

    Create LoginVM.cs
    Created a loginvm

---
+++ GameStoreApp/Data/ViewModel/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStoreApp.Data.ViewModel
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
