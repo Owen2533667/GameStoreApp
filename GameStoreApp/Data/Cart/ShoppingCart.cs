3c196a19d24fae76f04b1b2e76e4970fa23d1d1b
Owen2533667 <owenhughes67@gmail.com>
2023-04-24 01:06:55 +0100

    Create ShoppingCart.cs

---
+++ GameStoreApp/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,83 @@
+﻿using GameStoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStoreApp.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public GameStoreAppDbContext _context { get; set; }
+
+        public String ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(GameStoreAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public static ShoppingCart GetShoppingCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>().HttpContext.Session;
+            var context = services.GetService<GameStoreAppDbContext>();
+
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+            session.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+
+        public void AddItemToCart(Game game)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(x => x.Game.Id == game.Id && x.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Game = game,
+                    Amount = 1,
+                };
+
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            } else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _context.SaveChanges();
+        }
+
+        public void RemoveItemFromCart(Game game)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(x => x.Game.Id == game.Id && x.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                } else
+                {
+                    _context.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+            _context.SaveChanges();
+        }
+
+        public List<ShoppingCartItem> GetCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(x => x.ShoppingCartId == ShoppingCartId).Include(x => x.Game).ToList());
+        }
+
+        public double GetCartTotal() => _context.ShoppingCartItems.Where(x => x.ShoppingCartId == ShoppingCartId).Select(x => x.Game.Price * x.Amount).Sum();
+
+        public async Task ClearShoppingCartAsync()
+        {
+            var items = await _context.ShoppingCartItems.Where(x => x.ShoppingCartId == ShoppingCartId).ToListAsync();
+            _context.ShoppingCartItems.RemoveRange(items);
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
