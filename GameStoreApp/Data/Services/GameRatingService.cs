9501c4ac1c395401f5c8cacbd655d2fb99e673fc
Owen2533667 <owenhughes67@gmail.com>
2023-04-30 00:15:02 +0100

    Create GameRatingService.cs

---
+++ GameStoreApp/Data/Services/GameRatingService.cs
@@ -0,0 +1,13 @@
+﻿using GameStoreApp.Data.Base;
+using GameStoreApp.Models;
+
+namespace GameStoreApp.Data.Services
+{
+    public class GameRatingService : EntityBaseRepository<GameRating>, IGameRatingService
+    {
+        public GameRatingService(GameStoreAppDbContext context) : base(context)
+        {
+            
+        }
+    }
+}
