15082a9461ecd8d8eb3ab538bf7287411fa32b3b
Owen2533667 <owenhughes67@gmail.com>
2023-04-30 14:17:05 +0100

    added some comments
    added some comments

--- GameStoreApp/Data/Base/EntityBaseRepository.cs
+++ GameStoreApp/Data/Base/EntityBaseRepository.cs
@@ -5,19 +5,21 @@ using System.Linq.Expressions;
 
 namespace GameStoreApp.Data.Base
 {
+    //This will be a base repository with genereic implementation for all other service can use.
     public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
 
-        private readonly GameStoreAppDbContext _context;
-        public EntityBaseRepository(GameStoreAppDbContext context)
+        private readonly GameStoreAppDbContext _context; //Private readonly attribute of the dbcontext.
+        public EntityBaseRepository(GameStoreAppDbContext context) //constructor and inject the dbcontext.
         {
             _context = context;
         }
-
-        public async Task AddAsync(T entity)
+         
+        //Generic async method called AddAsync that returns a Task.
+        public async Task AddAsync(T entity) 
         {
-            await _context.Set<T>().AddAsync(entity);
-            await _context.SaveChangesAsync();
+            await _context.Set<T>().AddAsync(entity); //Set<T> is a genereic insert that will be replaced with the enetity being changed. Then add that enetity passed to the correct table.
+            await _context.SaveChangesAsync(); //saves the table.
         }
 
         //A gereneric implementation to delete data from the database. This method is asynchronous method that returns task. The method expects a parameter of type int that will be the id of the data being removed from the database.
@@ -30,24 +32,26 @@ namespace GameStoreApp.Data.Base
             await _context.SaveChangesAsync();
         }
 
-        //Asynchronous method to get all from the database table <T>
+        //Asynchronous method to get all from the database entity table of <T> and return that to a list. 
         public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
 
+        //this also returns a list of all records in a table, but accepts parameters to be included. 
         public async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties)
         {
             IQueryable<T> query = _context.Set<T>();
-            query = includeProperties.Aggregate(query, (current, includeProperties) => current.Include(includeProperties));
-            return await query.ToListAsync();
+            query = includeProperties.Aggregate(query, (current, includeProperties) => current.Include(includeProperties)); //aggregates of query including each of the parameter properties.
+            return await query.ToListAsync(); //returns that to a list.
         }
 
-        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(x => x.Id == id); //gets an entity with the provided id.
 
-        public async Task UpdateAsync(int id, T entity)
+        //Updates the entity with the id with the new entity data,
+        public async Task UpdateAsync(int id, T entity) 
         {
-            EntityEntry entityEntry = _context.Entry<T>(entity);
-            entityEntry.State = EntityState.Modified;
+            EntityEntry entityEntry = _context.Entry<T>(entity); //creates an instance of EntityEntry, which is used to to track changes.
+            entityEntry.State = EntityState.Modified; //sets the entity state to the modified state.
 
-            await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync(); //saves the changes.
         }
     }
 }
