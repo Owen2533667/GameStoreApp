e5a1b248025eef173d971d5ac65d8e7a4d28b8b8
Owen2533667 <owenhughes67@gmail.com>
2023-05-09 00:24:57 +0100

    Documentation and additional comments
    Added documentation of code and added additional comments to the code.

--- GameStoreApp/Controllers/GameController.cs
+++ GameStoreApp/Controllers/GameController.cs
@@ -22,9 +22,11 @@ namespace GameStoreApp.Controllers
             _service = service;
         }
 
-        //An async action method called index that accepts a parameter of type int called pg (Page), that will be defualt to one if not provided. This action method will be used to return
-        //the index view to the user. On the index view, there will be nine games that the user will be see. If the user clicks the next page of the pager, the next nine games will be displayed.
-        //Allows unautherized user to access this, even if the user is not logged in.
+        /// <summary>
+        /// The index action method will return the user to the index view with the collection of games from the database. Moreover, this view will act as an home page for the games e-commerece platform. This is an async method, that returns a task of IActionResult which will be the view Index for the game controller. It will send the game collection to the view as well. This method will allow anonymous users, meaning the user does not have to be authorised or logged in.
+        /// </summary>
+        /// <param name="pg">The page number of the current page. The default, if not provided, is one.</param>
+        /// <returns>Index view for the game controller, with the pagered collection of games. This should be nine games.</returns>
         [AllowAnonymous]
         public async Task<IActionResult> Index(int pg=1)
         {
@@ -95,28 +97,29 @@ namespace GameStoreApp.Controllers
         }
 
         /// <summary>
-        /// 
+        /// This Create action method, accepts one parameter of NewGameVM, will take the Create Form submit request and update the database with the form data. This will allow the creation of games by an admin user.
         /// </summary>
-        /// <param name="game"></param>
-        /// <returns></returns>
+        /// <param name="game">A NewGameVM, This will be the game that will be created and added to the database.</param>
+        /// <returns>If successful, redirects user to action method called index. If the model passed is not valid, return the create form and send back the game model data.</returns>
         [HttpPost]
         public async Task<IActionResult> Create(NewGameVM game)
         {
             //Checks to see if the game being created is valid. If not, send the user back to the create view with the select input data again, and the NewGameVM passed as parameter back.
             if (!ModelState.IsValid) 
             {
-                var gameDropdownData = await _service.GetNewGameDropDownValues();
+                var gameDropdownData = await _service.GetNewGameDropDownValues(); //Get the dropdown data for select inputs on the create form.
 
+                //Send this data using the viewbag.
                 ViewBag.Publishers = new SelectList(gameDropdownData.Publisher, "Id", "Name");
                 ViewBag.Developers = new SelectList(gameDropdownData.Developer, "Id", "Name");
                 ViewBag.VoiceActors = new SelectList(gameDropdownData.VoiceActor, "Id", "FullName");
                 ViewBag.Ratings = new SelectList(gameDropdownData.Rating, "Id", "Name");
                 ViewBag.Platform = new SelectList(gameDropdownData.Platform, "Id", "Name");
 
-                return View(game);
+                return View(game); //Send user to create form with the game model passed.
             }
 
-            await _service.AddNewGameAsync(game); //If the model is valid, then add the new game to the database.
+            await _service.AddNewGameAsync(game); //If the model is valid, then add the new game to the database. 
 
             return RedirectToAction("Index"); //Redirect the user to the index action.
         }
@@ -219,20 +222,21 @@ namespace GameStoreApp.Controllers
 
         //Get:
         /// <summary>
-        /// 
+        /// A delete action method that will take the authorised admin user to an delete conformation screen of a game with the id provided. This is an async method that will return a task of iactionresult which is the return of the delete view for games controller. 
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">The id of the game that the admin wants to delete.</param>
+        /// <returns>The delete view for the games controller with the game data.</returns>
         public async Task<IActionResult> Delete(int id)
         {
 
-            var data = await _service.GetGameByIdAsync(id);
+            var data = await _service.GetGameByIdAsync(id); //Get the game where the id passed to action matches game in the database id.
 
-            if (data == null) return View("NotFound");
+            if (data == null) return View("NotFound"); //if no game with id found, meaning the data is null, then return the view "NotFound".
 
-            var response = new NewGameVM()
+            var response = new NewGameVM() //creates an new instance of NewGameVM
             {
-                Id = data.Id,
+                //set the response (NewGameVM) instance properties to the game instance called data, this was retreived from the database using the id provided. 
+                Id = data.Id, 
                 Name = data.Name,
                 Description = data.Description,
                 Price = data.Price,
@@ -242,12 +246,12 @@ namespace GameStoreApp.Controllers
                 GameRatingId = data.GameRatingId,
                 GamePublisherId = data.GamePublisherId,
                 GameDeveloperId = data.GameDeveloperId,
-                VoiceActorIds = data.VoiceActors_Games.Select(x => x.VoiceActorId).ToList(),
-                PlatformIds = data.Platforms_Games.Select(x => x.PlatformId).ToList(),
+                VoiceActorIds = data.VoiceActors_Games.Select(x => x.VoiceActorId).ToList(), //Select all voice actor ids to a list.
+                PlatformIds = data.Platforms_Games.Select(x => x.PlatformId).ToList(), //Select all platform ids to a list.
             };
 
 
-            var gameDropdownData = await _service.GetNewGameDropDownValues();
+            var gameDropdownData = await _service.GetNewGameDropDownValues(); //get the dropdown data that will be displayed in the select inputs on the delete view. 
 
             ViewBag.Publishers = new SelectList(gameDropdownData.Publisher, "Id", "Name");
             ViewBag.Developers = new SelectList(gameDropdownData.Developer, "Id", "Name");
@@ -255,23 +259,23 @@ namespace GameStoreApp.Controllers
             ViewBag.Ratings = new SelectList(gameDropdownData.Rating, "Id", "Name");
             ViewBag.Platform = new SelectList(gameDropdownData.Platform, "Id", "Name");
 
-            return View(response);
+            return View(response); //return the delete view and send the response data.
         }
 
         /// <summary>
-        /// 
+        /// This action method will be called when the admin user clicks submit on the delete form in the delete view, conforming that they want to delte the game. This is an async method that returns an task of IActionResult and redirects the user to the action index.
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">The id of the game being deleted.</param>
+        /// <returns>If the game was deleted, then redirect to the action index. If the game id was not found, then returns "NotFound" view.</returns>
         [HttpPost, ActionName("Delete")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var dataDetails = await _service.GetGameByIdAsync(id);
-            if (dataDetails == null) return View("NotFound");
+            var dataDetails = await _service.GetGameByIdAsync(id); // get the game from the db which the id that matches the id passed.
+            if (dataDetails == null) return View("NotFound"); //if the data is null then return the "NotFound" View.
 
-            await _service.DeleteAsync(id);
+            await _service.DeleteAsync(id); //Delete the game from the db, with the id passed.
 
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Index)); //Redirect the user to the index action method.
         }
 
     }
--- GameStoreApp/Data/Cart/ShoppingCart.cs
+++ GameStoreApp/Data/Cart/ShoppingCart.cs
@@ -5,52 +5,88 @@ namespace GameStoreApp.Data.Cart
 {
     public class ShoppingCart
     {
-        public GameStoreAppDbContext _context { get; set; }
+        /// <summary>
+        /// The database context for the GameStoreApp.
+        /// </summary>
+        public GameStoreAppDbContext _context { get; set; } 
 
-        public String ShoppingCartId { get; set; }
-        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public String ShoppingCartId { get; set; } //A string that will represent the id of the shopping cart.
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; } //A list of shopping cart items that the user wants to buy.
 
+        /// <summary>
+        /// Initialises a new instance of the ShoppingCart class.
+        /// </summary>
+        /// <param name="context">The database context for the GameStoreApp.</param>
         public ShoppingCart(GameStoreAppDbContext context)
         {
+            // Set the database context for the shopping cart.
             _context = context;
         }
 
+        /// <summary>
+        /// Gets the shopping cart for the current user.
+        /// </summary>
+        /// <param name="services">The service provider to reteive the required service objects.</param>
+        /// <returns>A ShoppingCart object.</returns>
         public static ShoppingCart GetShoppingCart(IServiceProvider services)
         {
+            // Get the current user session object from the HttpContextAccessor.
             ISession session = services.GetRequiredService<IHttpContextAccessor>().HttpContext!.Session;
+
+            // Get the GameStoreAppDbContext object from the service provider.
             var context = services.GetService<GameStoreAppDbContext>();
 
+            // Get the cart ID from the current session. If it doesn't exist, create a new one.
             string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
             session.SetString("CartId", cartId);
 
-            return new ShoppingCart(context) { ShoppingCartId = cartId };
+            // Return a new ShoppingCart object with the GameStoreAppDbContext and setting the ShoppingCartId to cartId.
+            return new ShoppingCart(context!) { ShoppingCartId = cartId };
         }
 
+        /// <summary>
+        /// Adds a game item to the shopping cart.
+        /// </summary>
+        /// <param name="game">The game to add to the shopping cart.</param>
         public void AddItemToCart(Game game)
         {
+            // Get the shopping cart item, that also has the shopping cart id, for the specified game passed to method.
             var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(x => x.Game.Id == game.Id && x.ShoppingCartId == ShoppingCartId);
 
+            // If the shopping cart item doesn't exist, create a new one.
             if (shoppingCartItem == null)
             {
-                shoppingCartItem = new ShoppingCartItem()
+                shoppingCartItem = new ShoppingCartItem() //Create a new instance of ShoppingCartItem
                 {
-                    ShoppingCartId = ShoppingCartId,
-                    Game = game,
-                    Amount = 1,
+                    ShoppingCartId = ShoppingCartId, //Assign the shopping cart id to the current shopping cart id.
+                    Game = game, //Assign the game to the game passed
+                    Amount = 1, //Assign the amount to one.
                 };
 
-                _context.ShoppingCartItems.Add(shoppingCartItem);
-            } else
+                _context.ShoppingCartItems.Add(shoppingCartItem); // Add the new shopping cart item to the database.
+
+            }
+            else // Otherwise, increment the amount of the existing shopping cart item.
             {
+                // Increment the amount of the existing shopping cart item.
                 shoppingCartItem.Amount++;
             }
+
+            // Save changes to the database.
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Removes a game from the shopping cart.
+        /// </summary>
+        /// <param name="game">The game to remove from the shopping cart</param>
         public void RemoveItemFromCart(Game game)
         {
+            // Find the shopping cart item that corresponds to the specified game.
             var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(x => x.Game.Id == game.Id && x.ShoppingCartId == ShoppingCartId);
 
+            // If the shopping cart item exists, and if the amount is greate than one, decrement its amount by 1. If the amount is 0, remove the item from the shopping cart.
             if (shoppingCartItem != null)
             {
                 if (shoppingCartItem.Amount > 1)
@@ -61,25 +97,39 @@ namespace GameStoreApp.Data.Cart
                     _context.ShoppingCartItems.Remove(shoppingCartItem);
                 }
             }
+
+            // Save changes to the database.
             _context.SaveChanges();
         }
-
+        
+        /// <summary>
+        /// Returns the list of shopping cart items where it has the shopping cart id.
+        /// </summary>
+        /// <returns>A list of ShoppingCartItem objects.</returns>
         public List<ShoppingCartItem> GetCartItems()
         {
-            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(x => x.ShoppingCartId == ShoppingCartId).Include(x => x.Game).ToList());
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(x => x.ShoppingCartId == ShoppingCartId).Include(x => x.Game).ToList()); //Returns List<ShoppingCartItems>. If this is null ?? then assign values by getting the shopping cart items from the db where the shopping cart items, in the ShoppingCartItems table, have the ShoppingCartId, and then include the related game entity in the results. Then call ToList, to return a of List<ShoppingCartItems>.
         }
 
+        /// <summary>
+        /// Calculates the total cost of all items in the shopping cart.
+        /// </summary>
+        /// <returns> The total cost of all items in the shopping cart as a double</returns>
         public double GetCartTotal() => _context.ShoppingCartItems.Where(x => x.ShoppingCartId == ShoppingCartId).Select(x => x.Game.Price * x.Amount).Sum();
 
+        /// <summary>
+        /// An async method that clears the shopping cart items where they have the shopping cart id. This retreives the shopping cart id that has the matching shopping cart id and saves that List called items, and then removes a range, using the list of items,  of data from the db table ShoppingCartItems. This means that all shopping cart items with the shopping cart id will be removed from the db. It then set the ShoppingCartItems property to a new List of ShoppingCartItems
+        /// </summary>
+        /// <returns> A task that represents the asynchronous operation.</returns>
         public async Task ClearShoppingCartAsync()
         {
-            var items = await _context.ShoppingCartItems.Where(x => x.ShoppingCartId == ShoppingCartId).ToListAsync();
-            _context.ShoppingCartItems.RemoveRange(items);
-            await _context.SaveChangesAsync();
+            var items = await _context.ShoppingCartItems.Where(x => x.ShoppingCartId == ShoppingCartId).ToListAsync(); //Retreives the ShoppingCartItems from the db table ShoppingCartItems, where the ShoppingCartId equals the ShoppingCartId of the current cart and get these element to a List<ShoppingCartItems> and assign this to items.
 
-            ShoppingCartItems = new List<ShoppingCartItem>();
+            _context.ShoppingCartItems.RemoveRange(items); //Removes range of elements, using the items List<ShoppingCartItems> elements, from the table ShoppingCartItems.
+
+            await _context.SaveChangesAsync(); //Save the changes to the db.
+
+            ShoppingCartItems = new List<ShoppingCartItem>(); //Assing propety ShoppingCartItems to a new instance of List<ShoppingCartItem>
         }
-
-
     }
 }
