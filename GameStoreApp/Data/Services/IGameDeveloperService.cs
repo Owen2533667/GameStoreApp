e5238ce74cf04409c4f8911fb036f62a9d02901f
Owen <owenhughes67@gmail.com>
2023-05-09 14:12:44 +0100

    Documentation and comments

--- GameStoreApp/Data/Services/IGameDeveloperService.cs
+++ GameStoreApp/Data/Services/IGameDeveloperService.cs
@@ -3,9 +3,16 @@ using GameStoreApp.Models;
 
 namespace GameStoreApp.Data.Services
 {
+    /// <summary>
+    /// Represents a service for managing game developers.
+    /// </summary>
     public interface IGameDeveloperService : IEntityBaseRepository<GameDeveloper>
     {
-
+        /// <summary>
+        /// Gets a game developer by ID.
+        /// </summary>
+        /// <param name="id">The ID of the game developer.</param>
+        /// <returns>The game developer with the specified ID.</returns>
         Task<GameDeveloper> GetDeveloperByIdAsync(int id);
     }
 }
