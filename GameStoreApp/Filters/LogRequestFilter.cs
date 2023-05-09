4bfabc6330fb851359ac489a782f0a5b25ab72fa
Owen2533667 <owenhughes67@gmail.com>
2023-05-09 16:02:37 +0100

    Added a log filter
    This will be executed before the action method executes, and will be used to log the  controller name, controller action, IP address, current date and time, and the user making the request.

---
+++ GameStoreApp/Filters/LogRequestFilter.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Net.NetworkInformation;
+using System.Text;
+using GameStoreApp.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace GameStoreApp.Filters
+{
+    public class LogRequestFilter : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Called by the ASP.NET MVC framework before the action method executes.
+        /// </summary>
+        /// <param name="filterContext">The filter context.</param>
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Get the HTTP request object from the filter context.
+            var request = filterContext.HttpContext.Request;
+
+            // Create a new anonymous object called 'log' that contains information about the request.
+            var log = new
+            {
+                // Get the name of the controller that contains the action being executed.
+                Controller = filterContext.ActionDescriptor.RouteValues["controller"],
+                // Get the name of the action being executed.
+                Action = filterContext.ActionDescriptor.RouteValues["action"],
+                // Get the IP address of the client making the request as a string.
+                IP = filterContext.HttpContext.Connection.RemoteIpAddress?.ToString(),
+                // Get the current date and time in UTC format.
+                DateTime = DateTime.UtcNow,
+                // Get the name of the user making the request, if this is null, then assing "Guest"
+                User = filterContext.HttpContext.User.Identity?.Name ?? "Guest" 
+            };
+
+            // Write the JSON representation of 'log' to the debug output window.
+            Debug.WriteLine(JsonConvert.SerializeObject(log));
+        }
+    }
+}
